d131b014052cf19ea2985af7e783cc02e229ea8e
Parker Brown <brownpt98@gmail.com>
2023-11-03 23:45:36 -0500

    Increase vertical padding

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -8,9 +8,9 @@ type Props = {
 export default function Container({ children, padTop = true }: Props) {
   return (
     <div
-      className={classNames("max-w-7xl mx-auto px-4 pb-4 md:pb-16 md:px-16", {
+      className={classNames("max-w-7xl mx-auto px-4 pb-8 md:pb-16 md:px-16", {
         "pt-0": !padTop,
-        "pt-4 md:pt-16": padTop,
+        "pt-8 md:pt-16": padTop,
       })}
     >
       {children}
